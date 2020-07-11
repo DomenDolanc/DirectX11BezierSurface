451d9966226c233a78db5713fc048e9375cd67da
Domen Dolanc <domen.dolanc@dewesoft.com>
2020-07-11 09:20:27 +0200

    Remove additional comments

--- Bezier surface/Content/VertexShader.hlsl
+++ Bezier surface/Content/VertexShader.hlsl
@@ -1,4 +1,3 @@
-// A constant buffer that stores the three basic column-major matrices for composing geometry.
 cbuffer ModelViewProjectionConstantBuffer : register(b0)
 {
 	matrix model;
@@ -6,14 +5,12 @@ cbuffer ModelViewProjectionConstantBuffer : register(b0)
 	matrix projection;
 };
 
-// Per-vertex data used as input to the vertex shader.
 struct VertexShaderInput
 {
 	float3 pos : POSITION;
 	float3 color : COLOR0;
 };
 
-// Per-pixel color data passed through the pixel shader.
 struct PixelShaderInput
 {
 	float4 pos : SV_POSITION;
@@ -21,21 +18,15 @@ struct PixelShaderInput
 	float3 color : COLOR0;
 };
 
-// Simple shader to do vertex processing on the GPU.
 PixelShaderInput main(VertexShaderInput input)
 {
 	PixelShaderInput output;
 	float4 pos = float4(input.pos, 1.0f);
-
-	// Transform the vertex position into projected space.
 	pos = mul(pos, model);
 	pos = mul(pos, view);
 	pos = mul(pos, projection);
 	output.pos = pos;
-	
     output.controlPoint = input.pos.xz + 0.5f;
-
-	// Pass the color through without modification.
 	output.color = input.color;
 
 	return output;
